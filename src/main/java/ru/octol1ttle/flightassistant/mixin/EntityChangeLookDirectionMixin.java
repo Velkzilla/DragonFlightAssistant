14fe753452a5046cf896eb43e375278cd7fd5db5
Octol1ttle <l1ttleofficial@outlook.com>
2025-02-21 16:27:02 +0500

    do a barrel roll moment
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- src/main/java/ru/octol1ttle/flightassistant/mixin/EntityChangeLookDirectionMixin.java
+++ src/main/java/ru/octol1ttle/flightassistant/mixin/EntityChangeLookDirectionMixin.java
@@ -1,5 +1,6 @@
 package ru.octol1ttle.flightassistant.mixin;
 
+import dev.architectury.platform.Platform;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -22,6 +23,9 @@ abstract class EntityChangeLookDirectionMixin {
 
     @ModifyVariable(method = "changeLookDirection", at = @At("STORE"), ordinal = 1)
     private float overrideHeadingChange(float headingDelta) {
+        if (Platform.isModLoaded("do_a_barrel_roll") && Math.abs(headingDelta) >= 360.0f) {
+            headingDelta %= 360.0f;
+        }
         List<ControlInput> list = new ArrayList<>();
         ChangeLookDirectionEvents.HEADING.invoker().onChangeLookDirection(headingDelta, list);
         return Objects.requireNonNullElse(MixinHandlerKt.onEntityChangeHeading(list), headingDelta);
