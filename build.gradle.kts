5ac47c029e87376512d891d38b1355ac676f9302
Octol1ttle <l1ttleofficial@outlook.com>
2025-08-06 17:49:19 +0500

    config cache to make my life easier
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm")
     id("dev.isxander.modstitch.base") version "0.7.0-unstable"
     id("me.modmuss50.mod-publish-plugin")
-    id("me.fallenbreath.yamlang") version "1.4.1"
+    id("me.fallenbreath.yamlang") version "1.4.2"
 }
 
 fun prop(name: String) : String {
